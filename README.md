fd39184d833d5948d95bd14b942b2a5077b3d888
Nehalika Vadlamudi <nehalika100@gmail.com>
2024-07-18 08:52:02 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# My Portfolio
+
+### How to run this project on your machine?
+
+```bash
+Install php, composer, node from choco 
+
+Install Docker and StackBricks and provision postgres databse server
+
+Update .env according to your preference
+
+composer install && npm install && npm run build
+
+php artisan migrate && php artisan serve
+```
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Nehalika-Portfolio</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Clarence Taylor</span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/nanimoji.jpg" alt="..." /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- Page Content-->
+        <div class="container-fluid p-0">
+            <!-- About-->
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        Nehalika
+                        <span class="text-primary">Vadlamudi</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                         Mckinney , TX · (214) 699-9201 ·
+                        <a href="mailto:name@email.com">nehalika.work@gmail.com</a>
+                    </div>
+                    <p class="lead mb-5">I'm Creative <b>Web Application Developer</b>, with strong interest in Web Application Development.
+                        I enjoy turning complex problems into simple, beautiful and intuitive designs.</p>
+                        <p>
+                            My job is to build websites so that it is functional and user-friendly but at the same time attractive.
+                            Moreover, I
+                            add personal touch to your product and make sure that is eye-catching and easy to use. My aim is to bring
+                            across your
+                            message and identity in the most creative way. I created web design for many famous brand companies.
+                          </p>
+                          <p>I have experience with <b>HTML, CSS, JavaScript, React.js, Symfony, MySQL,Node JS, PHP, and AWS</b>. Beyond my work, I find satisfaction in stretching my horizons in terms of knowledge, keeping up with daily technical advancements, and exploring new places.</p>
+                    <div class="social-icons">
+                        <a class="social-icon" href="https://www.linkedin.com/in/nehalikav/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="social-icon" href="#!"><i class="fab fa-github"></i></a>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Experience-->
+            <section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Experience</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">PHP Web Developer</h3>
+                            <div class="subheading mb-3">Insomnia Cookies,PA</div>
+                            <p>Implemented Payment Gateway Integration, resulting in a 15% increase in completed transactions. Developed dynamic User Interfaces using PHP, Laravel, and Reactjs, leading to a 25% rise in user visits and a 30% improvement in order placements. Utilizing Eloquent ORM and Blade templating, optimized database interactions and improved page load times by 15%. Created efficient web services for seamless front-end and back-end communication and automated tasks using the Artisan tool. Designed Database Models and complex SQL queries for optimal data handling, and enhanced security and user experience with Middleware, reducing security incidents. Additionally, deployed applications on AWS, improving scalability and performance, actively participated in Agile methodologies to ensure project success, and integrated secure payment solutions, increasing successful transactions by 25% and user trust by 20%.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">November 2022 – Present</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">PHP Web Developer</h3>
+                            <div class="subheading mb-3">Ovia Health, MA</div>
+                            <p>Developed responsive user interfaces for Ovia Fertility with Reactjs and Tailwind, boosting engagement and revenue. Constructed web services for Ovia Pregnancy using PHP and Laravel, improving page load times by 25% and user retention by 12%. Optimized database models with Postgres, enhancing data management. Implemented PHPUnit tests, reducing post-deployment issues by 40%. Deployed applications on Azure for scalability. Collaborated in Agile development, contributing to sprint planning and fostering teamwork. Utilized Tailwind CSS for consistent UI and handled support tasks to maintain reliability. Employed Git and SVN for version control, reducing code conflicts by 30%. Conducted code reviews, improving quality by 25%. Worked with cross-functional teams to deliver high-quality applications. Proficient in Python, C, Node.js, Vue.js, Unix scripting, CI/CD processes, and Docker. Integrated PostgreSQL and Redis for efficient data storage and caching, strengthening project foundations with networking concepts.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">October 2020 - October 2022</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Web Developer</h3>
+                            <div class="subheading mb-3">Service Express, MI</div>
+                            <p>Constructed user interfaces with PHP, Symfony, Drupal, Angular, and Reactjs, enhancing user experience and SEO. Developed web services for Data Center Support, reducing lead-to-deal conversion by 10% and increasing deal closures by 25%. Designed database models with complex SQL queries, improving data storage and customer lifetime value (CMV). Automated database maintenance with bash scripting and ensured code quality with PHPUnit and Jasmine tests. Implemented CI/CD pipelines and deployed applications to Azure, reducing downtime by 15% and boosting customer satisfaction by 20%. Configured web servers with Apache. Collaborated in Agile teams and actively engaged in code reviews. Utilized Tailwind and CSS for user interfaces, enhancing upselling features. Improved application performance by 25% and customer engagement by 20% using PHP and Symfony. Integrated Headless Drupal, optimized data migrations, and engaged in social media outreach. Implemented AWS services (SQS, Lambda, ECS, EC2, RDS) and collaborated with QA teams for testing.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">June 2019 - September 2020</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Web Developer</h3>
+                            <div class="subheading mb-3">John Deere,IL</div>
+                            <p>Developed user-friendly interfaces with JQuery, JavaScript, and PHP. Built RESTful web services for JDLink™ Telematics System, improving data accessibility and real-time monitoring, resulting in a 12% increase in user visits and enhanced satisfaction. Designed optimized database models, reducing page load times and increasing retention and revenue. Wrote efficient SQL queries, boosting data processing by 30% and user interactions by 15%. Implemented tests for bug reduction and increased user trust and loyalty. Deployed applications to cloud for 99.9% uptime and scalability. Used Drupal and WordPress for enhanced functionalities and applied UX principles. Employed Git for efficient version control and automated deployment with Jenkins. Developed custom modules for improved engagement and media management. Participated in team mentoring, maintaining proficiency in NoSQL databases and utilizing LAMP stack and Kanban methodologies. Utilized Google Cloud Platform for business growth and scalability, emphasizing a full-stack approach to development.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">February 2018 - May 2019</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Software Developer</h3>
+                            <div class="subheading mb-3">Snapdeal, IND</div>
+                            <p>  Orchestrated seamless Product Listings with PHP and Symfony, increasing user engagement and product sales. Engineered robust web services for improved Search and Navigation accuracy, boosting product discovery and purchases by 12%. Formulated optimized database models for Seller Dashboard, reducing processing time by 30% and boosting new seller registrations. Ensured quality assurance in Order Management, reducing errors by 20% and enhancing customer satisfaction. Spearheaded cloud deployments for Payment Gateway Integration and applied SOLID principles for robust web applications. Implemented efficient version control with Github and automated build/deployment with Jenkins. Crafted responsive interfaces with jQuery, Bootstrap, CSS, and JavaScript for Customer Support. Engineered secure payment integration solutions, reducing disputes by 20% and increasing successful tracking by 15%. Executed projects with a structured Waterfall methodology for ongoing product feature releases.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">July 2013 - September 2016</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Education-->
+            <section class="resume-section" id="education">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Education</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">The University of Texas at Arlington</h3>
+                            <div class="subheading mb-3">Masters in Computer and Information Sciences(Arlington, Tx)</div>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">December 2017</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">ANU</h3>
+                            <div class="subheading mb-3">Bachelors in Computer Science & Engineering(India)</div>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">May 2013</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Skills-->
+            <section class="resume-section" id="skills">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Skills</h2>
+                    <div class="subheading mb-3">Programming Languages & Tools</div>
+                    <ul class="list-inline dev-icons">
+                        <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-angular"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-react"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-node-js"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-sass"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-less"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
+                        <li class="list-inline-item"><i class="fas fa-server"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-npm"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-php"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-docker"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-laravel"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-drupal"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-symfony"></i></li>
+                        <li class="list-inline-item"><i class="fas fa-database"></i></li>
+                        <li class="list-inline-item"><i class="fas fa-leaf"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-aws"></i></li>
+                    </ul>
+                    <div class="subheading mb-3">Workflow</div>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Mobile-First, Responsive Design
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Cross Browser Testing & Debugging
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Cross Functional Teams
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Agile Development & Scrum
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Interests-->
+            <section class="resume-section" id="interests">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Interests</h2>
+                    <p>Apart from being a web developer, I enjoy spending my time engaging in creative and relaxing activities. I love sketching and painting, which allow me to express my artistic side. Dancing is another passion of mine, as it keeps me active and brings joy to my life. I also enjoy visiting new places, which helps me broaden my horizons and experience different cultures.</p>
+                    <p class="mb-0">When at home, I practice yoga and meditation to maintain a balanced and peaceful state of mind. Additionally, I am a big fan of anime series, which I follow avidly in my free time.I am an aspiring chef, I also spend a significant amount of time exploring the latest technology advancements in the front-end web development world.</p>
+                </div>
+            </section>
+            <hr class="m-0" />
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
