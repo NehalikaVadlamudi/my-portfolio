461be379b9c30a9ffa351efed2d5aa857c9be7f4
Nehalika Vadlamudi <nehalika100@gmail.com>
2024-09-20 15:04:13 -0400

    updated porfolio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,10 +43,10 @@
                         <span class="text-primary">Vadlamudi</span>
                     </h1>
                     <div class="subheading mb-5">
-                         Mckinney , TX · (214) 699-9201 ·
+                         Mckinney , TX · (817) 313-9376 ·
                         <a href="mailto:name@email.com">nehalikavadlamudi@gmail.com</a>
                     </div>
-                    <p class="lead mb-5">I'm Creative <b>Web Application Developer</b>, with strong interest in Web Application Development.
+                    <p class="lead mb-5">I'm Creative <b>Application Developer</b>, with strong interest in Web Application Development.
                         I enjoy turning complex problems into simple, beautiful and intuitive designs.</p>
                         <p>
                             My job is to build websites so that it is functional and user-friendly but at the same time attractive.
@@ -55,7 +55,7 @@
                             across your
                             message and identity in the most creative way. I created web design for many famous brand companies.
                           </p>
-                          <p>I have experience with <b>HTML, CSS, JavaScript, React.js, Symfony, MySQL,Node JS, PHP, and AWS</b>. Beyond my work, I find satisfaction in stretching my horizons in terms of knowledge, keeping up with daily technical advancements, and exploring new places.</p>
+                          <p>I have experience with <b>HTML, CSS, JavaScript, React.js, Symfony, MySQL,Node JS, PHP, Docker,Python,Java,Postgres and AWS</b>. Beyond my work, I find satisfaction in stretching my horizons in terms of knowledge, keeping up with daily technical advancements, and exploring new places.</p>
                     <div class="social-icons">
                         <a class="social-icon" href="https://www.linkedin.com/in/nehalikav/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
                         <a class="social-icon" href="#!"><i class="fab fa-github"></i></a>
@@ -69,43 +69,27 @@
                     <h2 class="mb-5">Experience</h2>
                     <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
                         <div class="flex-grow-1">
-                            <h3 class="mb-0">PHP Web Developer</h3>
-                            <div class="subheading mb-3">Insomnia Cookies,PA</div>
-                            <p>Implemented Payment Gateway Integration, resulting in a 15% increase in completed transactions. Developed dynamic User Interfaces using PHP, Laravel, and Reactjs, leading to a 25% rise in user visits and a 30% improvement in order placements. Utilizing Eloquent ORM and Blade templating, optimized database interactions and improved page load times by 15%. Created efficient web services for seamless front-end and back-end communication and automated tasks using the Artisan tool. Designed Database Models and complex SQL queries for optimal data handling, and enhanced security and user experience with Middleware, reducing security incidents. Additionally, deployed applications on AWS, improving scalability and performance, actively participated in Agile methodologies to ensure project success, and integrated secure payment solutions, increasing successful transactions by 25% and user trust by 20%.</p>
+                            <h3 class="mb-0">Application Developer</h3>
+                            <div class="subheading mb-3">West East Integral(Weintergral)</div>
+                            <p>Developed and maintained the company’s web portal using HTML, CSS, PHP, and React.js, enhancing user interface and improving user experience by 20%. Leveraged Docker for containerization and Kubernetes for orchestration, resulting in optimized deployment processes and a 35% improvement in application scalability. Conducted comprehensive API testing with Postman, ensuring seamless backend integration and enhancing system reliability. Managed infrastructure across Linux/Unix and Windows environments, contributing to system stability and performance enhancements. Successfully deployed a consulting portal on Digital Ocean, reducing deployment times by 30% and decreasing manual errors by 50%. Implemented CI/CD pipelines with Jenkins, streamlining workflows and improving deployment efficiency by 40%. Integrated and managed data systems using PostgreSQL, Salesforce, and SugarCRM, ensuring robust data handling and system integration. Utilized Git for version control, enhancing collaboration and code management, leading to a 25% improvement in team productivity.</p>
                         </div>
-                        <div class="flex-shrink-0"><span class="text-primary">November 2022 – Present</span></div>
+                        <div class="flex-shrink-0"><span class="text-primary">February 2024 – Present</span></div>
                     </div>
                     <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
                         <div class="flex-grow-1">
-                            <h3 class="mb-0">PHP Web Developer</h3>
-                            <div class="subheading mb-3">Ovia Health, MA</div>
-                            <p>Developed responsive user interfaces for Ovia Fertility with Reactjs and Tailwind, boosting engagement and revenue. Constructed web services for Ovia Pregnancy using PHP and Laravel, improving page load times by 25% and user retention by 12%. Optimized database models with Postgres, enhancing data management. Implemented PHPUnit tests, reducing post-deployment issues by 40%. Deployed applications on Azure for scalability. Collaborated in Agile development, contributing to sprint planning and fostering teamwork. Utilized Tailwind CSS for consistent UI and handled support tasks to maintain reliability. Employed Git and SVN for version control, reducing code conflicts by 30%. Conducted code reviews, improving quality by 25%. Worked with cross-functional teams to deliver high-quality applications. Proficient in Python, C, Node.js, Vue.js, Unix scripting, CI/CD processes, and Docker. Integrated PostgreSQL and Redis for efficient data storage and caching, strengthening project foundations with networking concepts.</p>
+                            <h3 class="mb-0">Associate Consultant</h3>
+                            <div class="subheading mb-3">Neudesic</div>
+                            <p>Developed web interfaces for critical internal business functions, improving operational efficiency by 20%. Spearheaded the design of a user-centric educational platform for quiz-taking activities, collaborating with a product owner, which increased user engagement by 25%. Leveraged Angular, JavaScript, TypeScript, HTML5, and SCSS, reducing development time by 30% while maintaining consistent UI/UX quality. Integrated essential features such as Angular core, routes, forms, and the Microsoft Authentication Library, enhancing user security and reducing security issues by 15%. Collaborated with backend developers to optimize data retrieval and storage using MySQL, improving application performance and reliability by 40%. Implemented Git for version control, streamlining collaboration and improving code management across the team.</p>
                         </div>
-                        <div class="flex-shrink-0"><span class="text-primary">October 2020 - October 2022</span></div>
+                        <div class="flex-shrink-0"><span class="text-primary">September 2021 - October 2021</span></div>
                     </div>
                     <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
                         <div class="flex-grow-1">
-                            <h3 class="mb-0">Web Developer</h3>
-                            <div class="subheading mb-3">Service Express, MI</div>
-                            <p>Constructed user interfaces with PHP, Symfony, Drupal, Angular, and Reactjs, enhancing user experience and SEO. Developed web services for Data Center Support, reducing lead-to-deal conversion by 10% and increasing deal closures by 25%. Designed database models with complex SQL queries, improving data storage and customer lifetime value (CMV). Automated database maintenance with bash scripting and ensured code quality with PHPUnit and Jasmine tests. Implemented CI/CD pipelines and deployed applications to Azure, reducing downtime by 15% and boosting customer satisfaction by 20%. Configured web servers with Apache. Collaborated in Agile teams and actively engaged in code reviews. Utilized Tailwind and CSS for user interfaces, enhancing upselling features. Improved application performance by 25% and customer engagement by 20% using PHP and Symfony. Integrated Headless Drupal, optimized data migrations, and engaged in social media outreach. Implemented AWS services (SQS, Lambda, ECS, EC2, RDS) and collaborated with QA teams for testing.</p>
+                            <h3 class="mb-0">Junior Web Developer (Volunteer / Intern)</h3>
+                            <div class="subheading mb-3">RVR & JC College of Engineering</div>
+                            <p>Assisted in maintaining and enhancing the college's web portal, resulting in a 20% improvement in performance and user experience. Collaborated with professors and IT teams to troubleshoot issues, implement updates, and integrate Python-based solutions, which boosted system reliability by 25%. Developed front-end interfaces using HTML, CSS, and JavaScript, reducing page load times by 15% and improving usability. Leveraged Python for server-side scripting and automation, increasing backend processing efficiency by 30%. Optimized server-side logic and database interactions using PHP and MySQL, improving data handling capabilities by 25%. Integrated Python libraries for data analysis and API functions, enhancing user engagement by 10%. Spearheaded the Software Development Life Cycle (SDLC) of a student app, utilizing Java and MySQL to manage 35,000 signups, improving application performance and scalability by 20%. Administered Linux/Unix servers with 99.5% uptime, and automated tasks using Bash scripting, reducing manual interventions by 30% and improving system reliability. Implemented Git for version control, ensuring effective collaboration, and conducted unit testing to deliver a high-quality, reliable product.</p>
                         </div>
-                        <div class="flex-shrink-0"><span class="text-primary">June 2019 - September 2020</span></div>
-                    </div>
-                    <div class="d-flex flex-column flex-md-row justify-content-between">
-                        <div class="flex-grow-1">
-                            <h3 class="mb-0">Web Developer</h3>
-                            <div class="subheading mb-3">John Deere,IL</div>
-                            <p>Developed user-friendly interfaces with JQuery, JavaScript, and PHP. Built RESTful web services for JDLink™ Telematics System, improving data accessibility and real-time monitoring, resulting in a 12% increase in user visits and enhanced satisfaction. Designed optimized database models, reducing page load times and increasing retention and revenue. Wrote efficient SQL queries, boosting data processing by 30% and user interactions by 15%. Implemented tests for bug reduction and increased user trust and loyalty. Deployed applications to cloud for 99.9% uptime and scalability. Used Drupal and WordPress for enhanced functionalities and applied UX principles. Employed Git for efficient version control and automated deployment with Jenkins. Developed custom modules for improved engagement and media management. Participated in team mentoring, maintaining proficiency in NoSQL databases and utilizing LAMP stack and Kanban methodologies. Utilized Google Cloud Platform for business growth and scalability, emphasizing a full-stack approach to development.</p>
-                        </div>
-                        <div class="flex-shrink-0"><span class="text-primary">February 2018 - May 2019</span></div>
-                    </div>
-                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
-                        <div class="flex-grow-1">
-                            <h3 class="mb-0">Software Developer</h3>
-                            <div class="subheading mb-3">Snapdeal, IND</div>
-                            <p>  Orchestrated seamless Product Listings with PHP and Symfony, increasing user engagement and product sales. Engineered robust web services for improved Search and Navigation accuracy, boosting product discovery and purchases by 12%. Formulated optimized database models for Seller Dashboard, reducing processing time by 30% and boosting new seller registrations. Ensured quality assurance in Order Management, reducing errors by 20% and enhancing customer satisfaction. Spearheaded cloud deployments for Payment Gateway Integration and applied SOLID principles for robust web applications. Implemented efficient version control with Github and automated build/deployment with Jenkins. Crafted responsive interfaces with jQuery, Bootstrap, CSS, and JavaScript for Customer Support. Engineered secure payment integration solutions, reducing disputes by 20% and increasing successful tracking by 15%. Executed projects with a structured Waterfall methodology for ongoing product feature releases.</p>
-                        </div>
-                        <div class="flex-shrink-0"><span class="text-primary">July 2013 - September 2016</span></div>
+                        <div class="flex-shrink-0"><span class="text-primary">April 2020 - June 2021</span></div>
                     </div>
                 </div>
             </section>
@@ -119,14 +103,14 @@
                             <h3 class="mb-0">The University of Texas at Arlington</h3>
                             <div class="subheading mb-3">Masters in Computer and Information Sciences(Arlington, Tx)</div>
                         </div>
-                        <div class="flex-shrink-0"><span class="text-primary">December 2017</span></div>
+                        <div class="flex-shrink-0"><span class="text-primary">December 2023</span></div>
                     </div>
                     <div class="d-flex flex-column flex-md-row justify-content-between">
                         <div class="flex-grow-1">
-                            <h3 class="mb-0">ANU</h3>
+                            <h3 class="mb-0">RVR & JC College of Engineering</h3>
                             <div class="subheading mb-3">Bachelors in Computer Science & Engineering(India)</div>
                         </div>
-                        <div class="flex-shrink-0"><span class="text-primary">May 2013</span></div>
+                        <div class="flex-shrink-0"><span class="text-primary">June 2021</span></div>
                     </div>
                 </div>
             </section>
